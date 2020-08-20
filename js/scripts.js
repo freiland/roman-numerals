a4a9fc3a1b6274cbe1c215ab3387e8988a694d2c
Forrest Reiland <you@example.com>
2020-08-20 10:40:53 -0700

    add specs for roman numerals one through ten

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+const I = 1
+const V = 5
+const X =10 
+
+function romanLet(letter) {
+  if (letter === ("I", "V", "X",);
+  
+
+}
+const romanArray = [I,V,X]
+let romanArray = []
+for ()
+
+
+
+
+
+function oneToThree(number) {
+  
+}
+
+
+
+
+
+// If userNumInt <= 3 run the function "threeChar"//
+function threeChar(number) {
+ if (number === 1) {
+   console.log("I");
+ }
+ else if (number === 2) {
+   console.log("II");
+ }
+
+ else if (number === 3) {
+   console.log("III");
+ }
+}
+
+// If  10 > userNumInt > 3 //
+function subChar(number) {
+  if (number === 4) {
+    console.log("IV");
+  }
+  else if (number === 5) {
+    console.log("V");
+  }
+
+  else if (number === 6) {
+    console.log("VI");
+  }
+
+  
+}

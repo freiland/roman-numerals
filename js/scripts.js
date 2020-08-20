d9cb3607ba0f71616d9fa28f51d01a72e46c55b3
Forrest Reiland <you@example.com>
2020-08-20 11:22:29 -0700

    create value array with roman numerals assigned to numeric values

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
 const I = 1
 const V = 5
 const X =10 
+valueArr = ["I"=1, "V"=5, "X"=10, "L" = 50, "C"=100, "D"=500, "M"=1000];
 
 function romanLet(letter) {
-  if (letter === ("I", "V", "X",);
-  
+  if (letterI === ("I")); {
+  if (leterV === ("V")); {
+  if (letterX === ("X"));{
 
+  }
+  }
+  }
 }
-const romanArray = [I,V,X]
-let romanArray = []
-for ()
+const roman = "I"
+roman.replace(/I*/g, "1")
 
 
 
 
 
 function oneToThree(number) {
+  let oneArr = [];
+  let sumArr = [0]
   
+  while (sumArr <= number) {
+    oneArr.push(1);
+  }
+  sumArr = oneArr.reduce(function(a,b) {
+    return a+b;
+  });
+  
+  console.log(oneArr)
+
 }
 
 
